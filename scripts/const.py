34ad2c3c493da335c07cd8783debd7cf09c2f99b
Daniel Steinmaurer <dan.steinmaurer@gmail.com>
2025-05-05 21:07:53 +0200

    limit plotting area to 10^8

--- scripts/const.py
+++ scripts/const.py
@@ -5,8 +5,8 @@ DTYPE=np.float64
 EPS = np.finfo(DTYPE).eps
 
 START=1
-STOP=14
-N = 1000
+STOP=7.9
+N = 500
 P = np.logspace(START, STOP, N)
 
 mp.dps = 34 # approximate float128, which has a eps of ~1E-34
